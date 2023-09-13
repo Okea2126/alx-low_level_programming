f9937f091f22db40166f0bcdf0e2faa3bb987558
Okea2126 <ogunmolasegun95@gmail.com>
2023-09-13 06:46:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+        char ts;
+	int i;
+	i = 0;
+	while (i < 0)
+	{
+	for (ts = 'a'; ts <= 'z'; ts++)
+	{
+	_putchar(ts);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
