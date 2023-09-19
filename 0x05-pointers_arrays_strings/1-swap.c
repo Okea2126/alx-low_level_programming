7d4bf7779c09fda9cbe6771a30d8cb5156c95017
Okea2126 <ogunmolasegun95@gmail.com>
2023-09-18 17:49:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - my function name
+ * @a: first one parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
